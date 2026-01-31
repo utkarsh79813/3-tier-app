2d27d225750d188acc180aa1462e1bc30192c913
utkarsh79813 <162578396+utkarsh79813@users.noreply.github.com>
2026-01-31 19:35:17 +0530

    working frontend (#1)

--- myproject/Application-Code/frontend/src/services/taskServices.js
+++ myproject/Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
+const apiUrl = "http://98.92.172.51:30050/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
